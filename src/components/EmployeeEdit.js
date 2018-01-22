4c6d1946074087d6e39457a8939ee22f4c2f191c
Peter Robitaille <peterrobitaille@Peters-MacBook-Pro.local>
2018-01-22 09:27:58 -0500

    finished the app

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Communications from 'react-native-communications'
 import EmployeeForm from './EmployeeForm'
-import { employeeUpdate, employeeSave } from '../actions'
-import { Card, CardSection, Button } from './common'
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions'
+import { Card, CardSection, Button, Confirm } from './common'
 
 
 class EmployeeEdit extends Component {
+  state = { showModal: false }
 
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
@@ -27,6 +28,15 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`)
   }
 
+  onAccept() {
+    const { uid } = this.props.employee
+    this.props.employeeDelete({ uid })
+  }
+
+  onDecline() {
+    this.setState({ showModal: false })
+  }
+
   render() {
     return (
       <Card>
@@ -41,6 +51,18 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button onPress={this.onTextPress.bind(this)} text="Text Schedule" />
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })} text="Fire Employee" />
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     )
   }
@@ -52,4 +74,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift }
 }
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit)
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit)
